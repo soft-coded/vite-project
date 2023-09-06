cfc3189b0bd540c39fa7293e47a67ed16910f644
shrutanten <shrutanten.work@gmail.com>
2023-09-06 15:00:43 +0530

    Fix counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,27 +1,31 @@
-import React, { Component } from 'react';
+import { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 0
-    }
-    increment = () => {
-        this.setState({ count: this.state.count + 1 });
-    }
+	state = {
+		count: 0
+	};
 
-    decrement = () => {
-        if (this.state.count >= 1) {
-            this.setState({ count: this.state.count - 1 });
-        }
-    }
-    render() {
-        return (
-            <div className='d-flex justify-content-center mt-3'>
-                <input type="button" onClick={this.increment} value="Increment" className="btn btn-primary" />
-                <h3 className='text-center mx-3'>{this.state.count}</h3>
-                <input type="button" onClick={this.decrement} value="Decrement" className='btn btn-secondary' />
-            </div>
-        );
-    }
+	increment = () => {
+		this.setState({ count: this.state.count + 1 });
+	};
+
+	decrement = () => {
+		this.setState({ count: this.state.count - 1 });
+	};
+
+	render() {
+		return (
+			<div className="d-flex justify-content-center mt-3">
+				<button onClick={this.increment} className="btn btn-primary">
+					Increment
+				</button>
+				<h3 className="text-center mx-3">{this.state.count}</h3>
+				<button onClick={this.decrement} className="btn btn-secondary">
+					Decrement
+				</button>
+			</div>
+		);
+	}
 }
 
 export default Counter;
