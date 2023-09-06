29b696b1ddcd402e677a1a1467b08a6c0b154faa
shrutanten <shrutanten.work@gmail.com>
2023-09-06 14:45:29 +0530

    Fix app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import { Nav } from './components/nav'
-import Login from './components/login';
-import Counter from './components/counter';
-import PageNotFound from './components/pageNotFound';
-import Products from './components/products';
-import Home from './components/home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Nav } from "./components/nav";
+import Login from "./components/login";
+import Counter from "./components/counter";
+import PageNotFound from "./components/pageNotFound";
+import Products from "./components/products";
+import Home from "./components/home";
 
 function App() {
-
-
-  return (<React.Fragment>
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path='' element={<Home />} />,
-        <Route path='login' element={<Login />} />,
-        <Route path='products' element={<Products />} />,
-        <Route path='counter' element={<Counter />} />,
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </React.Fragment >)
+	return (
+		<BrowserRouter>
+			<Nav />
+			<Routes>
+				<Route path="" element={<Home />} />,
+				<Route path="login" element={<Login />} />,
+				<Route path="products" element={<Products />} />,
+				<Route path="counter" element={<Counter />} />,
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
-
-
+export default App;
