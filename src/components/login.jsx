3c123abb1f320cc402a3d337e66f680b7408dc1c
shrutanten <shrutanten.work@gmail.com>
2023-09-07 12:25:04 +0530

    Convert class component

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,84 +1,66 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class Login extends Component {
-	state = {
-		user: {
-			email: "",
-			password: ""
-		}
-	};
+function Login() {
+	const [user, setUser] = useState({
+		email: "",
+		password: ""
+	});
 
-	// handle change method - to capture form data
-	handleChange = event => {
-		//console.log(event);
-		// name of the field user selected
-		console.log(event.target.name);
+	function handleChange(event) {
+		setUser({ email: event.target.value });
+	}
 
-		// value entered by user
-		console.log(event.target.value);
-
-		// copy user object info into loginUser
-		const loginUser = { ...this.state.user };
-
-		// Update form data entered by user in loginUser object
-		loginUser[event.target.name] = event.target.value;
-
-		// Update user property defined in state object with loginUser data
-		this.setState({ user: loginUser });
-	};
-
-	handleSubmit = event => {
+	function handleSubmit(event) {
 		event.preventDefault();
 		console.log("handleSubmit");
 		console.log(this.state.user);
 
 		// logic to authenticate user
-	};
-	render() {
-		return (
-			<div
-				className="mx-auto border p-3 mt-5"
-				style={{ minWidth: "300px", maxWidth: "600px" }}
-			>
-				<h4>Login</h4>
-				<hr />
-				<form onSubmit={this.handleSubmit}>
-					<div className="mb-3">
-						<label htmlFor="exampleInputEmail1" className="form-label">
-							Email address
-						</label>
-						<input
-							onChange={this.handleChange}
-							value={this.state.user.email}
-							name="email"
-							type="email"
-							className="form-control"
-							id="exampleInputEmail1"
-							aria-describedby="emailHelp"
-						/>
-					</div>
-					<div className="mb-3">
-						<label htmlFor="exampleInputPassword1" className="form-label">
-							Password
-						</label>
-						<input
-							onChange={this.handleChange}
-							value={this.state.user.password}
-							name="password"
-							type="password"
-							className="form-control"
-							id="exampleInputPassword1"
-						/>
-					</div>
-					<div className="d-grid gap-2">
-						<button type="submit" className="btn btn-primary">
-							Submit
-						</button>
-					</div>
-				</form>
-			</div>
-		);
 	}
+
+	return (
+		<div
+			className="mx-auto border p-3 mt-5"
+			style={{ minWidth: "300px", maxWidth: "600px" }}
+		>
+			<h4>Login</h4>
+			<hr />
+			<form onSubmit={this.handleSubmit}>
+				<div className="mb-3">
+					<label htmlFor="exampleInputEmail1" className="form-label">
+						Email address
+					</label>
+					<input
+						onChange={this.handleChange}
+						value={user.email}
+						name="email"
+						type="email"
+						className="form-control"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+					/>
+				</div>
+				<div className="mb-3">
+					<label htmlFor="exampleInputPassword1" className="form-label">
+						Password
+					</label>
+					<input
+						onChange={this.handleChange}
+						value={this.state.user.password}
+						name="password"
+						type="password"
+						className="form-control"
+						id="exampleInputPassword1"
+					/>
+				</div>
+				<div className="d-grid gap-2">
+					<button type="submit" className="btn btn-primary">
+						Submit
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default Login;
