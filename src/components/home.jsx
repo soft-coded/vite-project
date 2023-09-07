e8e5c6789647ca7e3aedc364291d255ca5e3cb93
shrutanten <shrutanten.work@gmail.com>
2023-09-07 12:29:49 +0530

    Fix errors

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
-
-const Home = (props) => {
-
-    return (
-        <div>
-            <h1>Home Component</h1>
-        </div>
-    );
-}
+const Home = () => {
+	return (
+		<div>
+			<h1 style={{ textAlign: "center" }}>Home Component</h1>
+		</div>
+	);
+};
 
 export default Home;
